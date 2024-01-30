c47fde464b32bb3a0d16601196945d18c1fcbc46
Curtis Gray <curtis@carverlab.com>
2024-01-30 12:33:36 -0500

    remove unused ux_proto folder

--- ux_proto/utils/server/orm.ts
+++
@@ -1,8 +0,0 @@
-// import * as orm from "./orm.Sqlite";
-import { default as ormSqlite } from "./orm.Sqlite.object";
-
-// //orm.initializeOrm().then(async () => { }).catch((err) => { console.error(err); });
-
-// ormSqlite().initializeOrm().then(async () => { }).catch((err: Error|string) => { console.error(err); });
-
-export default ormSqlite;
