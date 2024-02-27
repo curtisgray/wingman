3b040e76fbf72a834712738b194bbe491bf0c1de
Curtis Gray <curtis@carverlab.com>
2024-02-27 13:50:18 -0500

    reworked next startup for macos and windows

--- ux/main.js
+++ ux/main.js
@@ -12,9 +12,12 @@ const winston = require("winston");
 const net = require("net");
 const si = require('systeminformation');
 const url = require('url');
+const next = require('next');
+const http = require('http');
 const { t } = require("i18next");
 
 const APP_DIR = path.join(os.homedir(), ".wingman");
+
 const logger = winston.createLogger({
     level: "silly",
     format: winston.format.printf(({ level, message, label, timestamp }) => {
@@ -89,12 +92,49 @@ const etell = (data) => {
     logger.log("error", data);
 };
 
-const startNextJsServer = async (port) => {
-    return new Promise((resolve, reject) => {
-        logger.log("silly", "Starting Next.js server");
-        const baseDir = app.isPackaged ? process.resourcesPath : __dirname;
-        tell(`Base directory: ${baseDir}`);
-        const nextDir = path.join(baseDir, ".next");
+const getBaseDir = () => {
+    let baseDir = path.join(__dirname, 'server');
+    // check if the base directory exists, or this is running on macos, if not, use the resources path
+    if (!fs.existsSync(baseDir)) {
+        etell(`Base directory does not exist: ${baseDir}. Using Resources path.`);
+        baseDir = path.dirname(process.resourcesPath);
+    } else if (process.platform === 'darwin') {
+        tell(`Running on MacOS. Using Resources path`);
+        baseDir = path.resolve(path.dirname(path.join(__dirname, '..', 'Resources')));
+    }
+    return baseDir;
+};
+
+const startNextJs = (port, nextDir) => {
+  return new Promise((resolve, reject) => {
+    tell("Starting Next.js server");
+
+    const dev = process.env.NODE_ENV !== 'production';
+    const nextApp = next({ dev, conf: { distDir: nextDir } });
+    const handleNextRequests = nextApp.getRequestHandler();
+
+    nextApp.prepare()
+      .then(() => {
+        const server = http.createServer((req, res) => handleNextRequests(req, res));
+
+        server.listen(port, (err) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+            return;
+          }
+          console.log(`> Ready on http://localhost:${port}`);
+          resolve(server);
+        });
+      })
+      .catch(reject);
+  });
+};
+
+const startNextJsServer = async (port, nextDir) => {
+    return new Promise(async (resolve, reject) => {
+        tell("Starting Next.js server");
+
         if (!fs.existsSync(nextDir)) {
             const errMsg = "The .next directory does not exist. Something went wrong. Exiting...";
             etell(errMsg);
@@ -107,6 +147,7 @@ const startNextJsServer = async (port) => {
         tell(`Starting Next.js server: ${exe} ${args.join(" ")}`);
         serverProcess = child_process.spawn(exe, args, {
             cwd: baseDir,
+            // cwd: __dirname,
             stdio: ["inherit", "pipe", "pipe"], // Change stdio to pipe for stdout and stderr
             shell: true,
             windowsHide: true,
@@ -137,11 +178,9 @@ const startNextJsServer = async (port) => {
     });
 };
 
-const launchWingmanExecutable = async () => {
+const launchWingmanExecutable = async (baseDir) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let platform = os.platform();
-            let architecture = os.arch();
             let executableName = 'wingman';
             let useCublas = false;
 
@@ -153,43 +192,38 @@ const launchWingmanExecutable = async () => {
                 useCublas = true;
             }
 
-            const baseDir = app.isPackaged ? process.resourcesPath : path.join(__dirname, 'server');
-            tell(`Base directory: ${baseDir}`);
+            tell(`Wingman Base directory: ${baseDir}`);
             let executablePath = "";
             let cwd = "";
-            switch (platform) {
+            let wingman_runtime = "";
+            switch (process.platform) {
                 case 'win32':
-                    platform = useCublas ? 'windows-cublas' : 'windows';
+                    wingman_runtime = useCublas ? 'windows-cublas' : 'windows';
                     executableName += '.exe';
-                    cwd = path.join(baseDir, 'wingman', platform, 'bin');
-                    executablePath = path.join(cwd, executableName);
                     break;
                 case 'linux':
-                    platform = useCublas ? 'linux-cublas' : 'linux';
-                    cwd = path.join(baseDir, 'wingman', platform, 'bin');
-                    executablePath = path.join(cwd, executableName);
+                    wingman_runtime = useCublas ? 'linux-cublas' : 'linux';
                     break;
                 case 'darwin':
-                    if (architecture === 'arm64') {
-                        platform = 'macos-metal';
+                    if (process.arch === 'arm64') {
+                        wingman_runtime = 'macos-metal';
                     } else {
-                        platform = 'macos';
+                        wingman_runtime = 'macos';
                     }
-                    // run the macOS version of the wingman executable
-                    cwd = path.join(baseDir, 'wingman', platform, 'bin', 'wingman.app', 'Contents', 'MacOS');
-                    executablePath = path.join(cwd, executableName);
                     break;
                 default:
-                    reject(new Error(`Unsupported platform: ${platform}`));
+                    reject(new Error(`Unsupported platform: ${process.platform}`));
                     return;
             }
-            tell(`Platform: ${platform}`);
+            cwd = path.join(baseDir, 'wingman', wingman_runtime, 'bin');
+            executablePath = path.join(cwd, executableName);
+            tell(`Wingman Runtime: ${wingman_runtime}`);
 
             tell(`Launching Wingman executable: ${executablePath}`);
             const subprocess = child_process.spawn(executablePath, [], {
                 stdio: ['inherit', 'pipe', 'pipe'],
                 shell: true,
-                cwd: path.join(baseDir, 'wingman', platform, 'bin'),
+                cwd: path.join(baseDir, 'wingman', wingman_runtime, 'bin'),
                 windowsHide: true,
             });
 
@@ -240,8 +274,12 @@ const createWindow = () => {
     findPort((current, total) => tell(`Checked ${current} of ${total} ports.`))
         .then(async (port) => {
             tell(`Found an open port: ${port}`);
-            processes.push(await launchWingmanExecutable());
-            processes.push(await startNextJsServer(port));
+            const baseDir = getBaseDir();
+            tell(`Base directory: ${baseDir}`);
+            const nextDir = path.join(baseDir, ".next");
+            tell(`Next.js directory: ${nextDir}`);
+            processes.push(await launchWingmanExecutable(baseDir));
+            processes.push(await startNextJs(port, nextDir));
             win.loadURL(url.format({
                 pathname: `localhost:${port}`,
                 protocol: 'http:',
@@ -275,6 +313,7 @@ app.whenReady().then(() => {
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") app.quit();
     for (let i = 0; i < processes.length; i++) {
-        processes[i].kill();
+        // processes[i].kill();
+        processes[i].close();
     }
 });
