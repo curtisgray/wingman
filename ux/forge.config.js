38c34766232de233e09048088ef6e50c83bb9979
Curtis Gray <curtis@carverlab.com>
2024-03-04 09:24:39 -0500

    Fix EF publishers entry in config

--- ux/forge.config.js
+++ ux/forge.config.js
@@ -39,7 +39,7 @@ module.exports = {
     ],
     publishers: [
         {
-            name: "@curtisgray/wingman",
+            name: '@electron-forge/publisher-github',
             config: {
                 repository: {
                     owner: "curtisgray",
