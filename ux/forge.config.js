274bb5a57ddbbf61ec32c6624c9ce690f4f1654e
Curtis Gray <curtis@carverlab.com>
2024-03-17 03:31:32 -0400

    Add tool to osx notarize

--- ux/forge.config.js
+++ ux/forge.config.js
@@ -14,6 +14,7 @@ module.exports = {
         icon: "assets/logo-color",
         osxSign: {},
         osxNotarize: {
+            tool: 'notarytool',
             appleId: process.env.APPLE_ID,
             appleIdPassword: process.env.APPLE_PASSWORD,
             teamId: process.env.APPLE_TEAM_ID
