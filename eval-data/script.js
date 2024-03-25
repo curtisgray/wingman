97ea1c5513268d82add187ae70f4305b2afbe0b0
Curtis Gray <curtis@carverlab.com>
2024-03-25 08:20:43 -0400

    added eval data and change button colors

---
+++ eval-data/script.js
@@ -0,0 +1,515 @@
+const leaderboardDataEqbench = `model,score,params
+internlm/internlm2-chat-7b,62.61,7
+NousResearch/Nous-Hermes-2-Yi-34B,72.68,34
+Yhyu13/LMCocktail-10.7B-v1,73.67,10.7
+01-ai/Yi-34B-Chat,71.62,34
+Open-Orca/Mistral-7B-OpenOrca,66.55,7
+fblgit/una-cybertron-7b-v2-bf16,62.83,7
+Intel/neural-chat-7b-v3-1,64.77,7
+Toten5/Marcoroni-neural-chat-7B-v2,68.54,7
+huggingfaceh4/zephyr-7b-beta,58.33,7
+madatnlp/marcoroni-7b-v3-safetensor,71.68,7
+mistralai/mistral-7b-instruct-v0.1,52.15,7
+Weyaxi/SauerkrautLM-UNA-SOLAR-Instruct,73.56,10.7
+huggingfaceh4/zephyr-7b-alpha,56.82,7
+meta-llama/Llama-2-13b-chat-hf,49.12,13
+zyh3826/GML-Mistral-merged-v1,74.01,7
+upstage/SOLAR-10.7B-Instruct-v1.0,73.53,10.7
+cognitivecomputations/dolphin-2_2-yi-34b,75.52,34
+cognitivecomputations/dolphin-2.2-70b,79.6,70
+gpt-4-0314,85.73,
+gpt-4-0613,84.79,
+gpt-4-1106-preview,86.05,
+TheBloke/koala-7B-HF,21.54,7
+meta-llama/Llama-2-70b-chat-hf,73.59,70
+lmsys/vicuna-7b-v1.1,26.12,7
+NousResearch/Nous-Capybara-7B-V1,34.37,7
+mistral-medium,82.57,
+meta-llama/Llama-2-7b-chat-hf,36.32,7
+gemini-pro,75.08,
+migtissera/SynthIA-70B-v1.5,73.71,70
+openchat/openchat-3.5-1210,72.52,7
+openchat/openchat_3.5,72.18,7
+mlabonne/Beagle14-7B,74.45,7
+mlabonne/NeuralMarcoro14-7B,74.15,7
+YeungNLP/firefly-mixtral-8x7b,64.36,8x7
+mlabonne/NeuralHermes-2.5-Mistral-7B,65.86,7
+cloudyu/Mixtral_34Bx2_MoE_60B,72.69,34x2
+mistralai/Mixtral-8x7B-Instruct-v0.1,72.37,8x7
+mistralai/Mistral-7B-Instruct-v0.2,68.18,7
+lxuechen/phi-2-dpo,54.42,2.7
+rhysjones/phi-2-orange,56.94,2.7
+mlabonne/phixtral-2x2_8,54.58,2x2.7
+microsoft/phi-2,27.6,2.7
+mlabonne/Beyonder-4x7B-v2,69.23,4x7
+gpt-3.5-turbo-1106,71.74,
+gpt-3.5-turbo-0613,69.35,
+gpt-3.5-turbo-0301,70.67,
+rishiraj/meow,73.94,10.7
+alpindale/goliath-120b,76.09,120
+migtissera/Tess-XL-v1.0,78.46,120
+mlabonne/NeuralBeagle14-7B,74.79,7
+NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT,72.91,8x7
+vince62s/phi-2-psy,56.44,2.7
+stabilityai/stablelm-2-zephyr-1_6b,15.04,1.6
+cognitivecomputations/MegaDolphin-120b,80.21,120
+OrionStarAI/Orion-14B-Chat,59.71,14
+cognitivecomputations/laserxtral,71.96,4x7
+macadeliccc/SOLAR-10.7b-Instruct-dpo,73.21,10.7
+tiiuae/falcon-180B-chat,56.82,180
+Qwen/Qwen-1_8B-Chat,30,1.8
+Qwen/Qwen-14B-Chat,63.47,14
+Qwen/Qwen-7B-Chat,50.11,7
+01-ai/Yi-6B-Chat,61.79,6
+miqudev/miqu-1-70b,82.91,70
+DiscoResearch/DiscoLM-120b,78.48,120
+Qwen/Qwen-72B-Chat,80.7,72
+WizardLM/WizardLM-70B-V1.0,71.28,70
+lmsys/vicuna-13b-v1.5,67.39,13
+allenai/tulu-2-dpo-70b,76.63,70
+WizardLM/WizardLM-13B-V1.2,63.71,13
+cognitivecomputations/dolphin-2.2.1-mistral-7b,69.92,7
+timdettmers/guanaco-33b-merged,36.11,33
+teknium/OpenHermes-2.5-Mistral-7B,66.89,7
+berkeley-nest/Starling-LM-7B-alpha,73.9,7
+lmsys/vicuna-33b-v1.3,67.07,33
+serpdotai/sparsetral-16x7B-v2,59.9,9
+Qwen/Qwen1.5-14B-Chat,74.99,14
+Qwen/Qwen1.5-4B-Chat,28.75,4
+Qwen/Qwen1.5-1.8B-Chat,24.12,1.8
+Qwen/Qwen1.5-72B-Chat,82.81,72
+Qwen/Qwen1.5-7B-Chat,54.41,7
+vilm/Quyen-Pro-Max-v0.1,77.16,72
+ShinojiResearch/Senku-70B-Full,84.89,70
+claude-instant-1.2,69.04,
+claude-2.1,73.96,
+claude-1,76.83,
+claude-2.0,72.89,
+pplx-70b-online,62.79,70
+pplx-7b-online,48.91,7
+snorkelai/Snorkel-Mistral-PairRM-DPO,65.83,7
+alpindale/miquella-120b,82.15,120
+wolfram/miquliz-120b-v2.0,82.21,120
+migtissera/Tess-72B-v1.5b,81.78,72
+vilm/Quyen-Pro-v0.1,70.75,14
+mlabonne/Monarch-7B,75.8,7
+mlabonne/NeuralMonarch-7B,76.26,7
+mlabonne/AlphaMonarch-7B,76.08,7
+gpt-3.5-turbo-0125,64.97,
+gpt-4-0125-preview,83.87,
+google/gemma-7b-it,61.72,7
+google/gemma-2b-it,23.26,2
+senseable/WestLake-7B-v2,78.7,7
+abacusai/Smaug-72B-v0.1,79.75,72
+abacusai/TheProfessor-155b,78.82,155
+mistral-small-2402,80.36,
+mistral-large-2402,85.17,
+yam-peleg/Experiment26-7B,77.21,7
+cognitivecomputations/Samantha-120b,76.44,120
+sophosympatheia/Midnight-Miqu-70B-v1.0,75.9,70
+claude-3-sonnet-20240229,80.45,
+claude-3-opus-20240229,82.19,
+*Infinimol/miiqu-f16,83.17,105,
+*mlabonne/Beyonder-4x7B-v3,77.01,4x7`;
+
+const leaderboardDataMagi = `model,score
+TheBloke/koala-7B-HF,23.7
+lmsys/vicuna-7b-v1.1,27.38
+stabilityai/stablelm-2-zephyr-1_6b,27.54
+lmsys/vicuna-13b-v1.5,28.75
+Qwen/Qwen-1_8B-Chat,29.19
+NousResearch/Nous-Capybara-7B-V1,30.16
+mlabonne/phixtral-2x2_8,30.44
+microsoft/phi-2,30.57
+mistralai/mistral-7b-instruct-v0.1,30.69
+Qwen/Qwen1.5-1.8B-Chat,31.56
+lmsys/vicuna-33b-v1.3,31.66
+timdettmers/guanaco-33b-merged,31.78
+lxuechen/phi-2-dpo,31.85
+rhysjones/phi-2-orange,32.03
+vince62s/phi-2-psy,32.03
+Qwen/Qwen1.5-4B-Chat,32.66
+cognitivecomputations/dolphin-2.2.1-mistral-7b,33.16
+Qwen/Qwen-7B-Chat,33.44
+mistralai/Mistral-7B-Instruct-v0.2,34.69
+serpdotai/sparsetral-16x7B-v2,34.97
+huggingfaceh4/zephyr-7b-alpha,35.15
+Open-Orca/Mistral-7B-OpenOrca,35.78
+huggingfaceh4/zephyr-7b-beta,35.97
+Toten5/Marcoroni-neural-chat-7B-v2,36.31
+senseable/WestLake-7B-v2,36.59
+Intel/neural-chat-7b-v3-1,36.65
+berkeley-nest/Starling-LM-7B-alpha,37.06
+mlabonne/NeuralMarcoro14-7B,37.12
+teknium/OpenHermes-2.5-Mistral-7B,37.31
+openchat/openchat_3.5,37.34
+cognitivecomputations/laserxtral,37.46
+fblgit/una-cybertron-7b-v2-bf16,37.5
+snorkelai/Snorkel-Mistral-PairRM-DPO,37.53
+mlabonne/NeuralHermes-2.5-Mistral-7B,37.56
+mlabonne/Beyonder-4x7B-v2,38.03
+internlm/internlm2-chat-7b,38.43
+01-ai/Yi-6B-Chat,38.74
+openchat/openchat-3.5-1210,38.81
+mlabonne/AlphaMonarch-7B,39.12
+mlabonne/Monarch-7B,39.56
+mlabonne/NeuralMonarch-7B,39.59
+upstage/SOLAR-10.7B-Instruct-v1.0,39.62
+Qwen/Qwen-14B-Chat,39.74
+WizardLM/WizardLM-70B-V1.0,39.87
+gpt-3.5-turbo-0613,40.55
+mlabonne/Beagle14-7B,41.02
+mlabonne/NeuralBeagle14-7B,41.06
+zyh3826/GML-Mistral-merged-v1,41.18
+Qwen/Qwen1.5-7B-Chat,41.59
+Weyaxi/SauerkrautLM-UNA-SOLAR-Instruct,42.43
+YeungNLP/firefly-mixtral-8x7b,42.46
+Yhyu13/LMCocktail-10.7B-v1,42.65
+gpt-3.5-turbo-0125,42.65
+rishiraj/meow,42.68
+gpt-3.5-turbo-1106,43.17
+mistralai/Mixtral-8x7B-Instruct-v0.1,45.74
+gpt-3.5-turbo-0301,46.66
+gemini-pro,46.87
+migtissera/Tess-XL-v1.0,48.08
+migtissera/SynthIA-70B-v1.5,48.92
+cognitivecomputations/dolphin-2.2-70b,49.73
+allenai/tulu-2-dpo-70b,50.23
+NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT,51.83
+mistral-small-2402,51.9
+cognitivecomputations/MegaDolphin-120b,54.45
+01-ai/Yi-34B-Chat,57.1
+Qwen/Qwen-72B-Chat,60.38
+cognitivecomputations/dolphin-2_2-yi-34b,60.66
+mistral-medium,62.15
+NousResearch/Nous-Hermes-2-Yi-34B,63.03
+cloudyu/Mixtral_34Bx2_MoE_60B,65.06
+mistral-large-2402,67.69
+gpt-4-1106-preview,74.96
+gpt-4-0314,75.67
+claude-3-opus-20240229,76.55
+gpt-4-0125-preview,76.83
+gpt-4-0613,77.85
+madatnlp/marcoroni-7b-v3-safetensor,37.06
+meta-llama/Llama-2-13b-chat-hf,28.2
+meta-llama/Llama-2-70b-chat-hf,35.4
+meta-llama/Llama-2-7b-chat-hf,27.5
+alpindale/goliath-120b,50.36
+OrionStarAI/Orion-14B-Chat,40.74
+macadeliccc/SOLAR-10.7b-Instruct-dpo,42.37
+miqudev/miqu-1-70b,63.22
+DiscoResearch/DiscoLM-120b,54.01
+WizardLM/WizardLM-13B-V1.2,29.1
+Qwen/Qwen1.5-14B-Chat,49.27
+Qwen/Qwen1.5-72B-Chat,63.47
+vilm/Quyen-Pro-Max-v0.1,59.29
+ShinojiResearch/Senku-70B-Full,63.94
+alpindale/miquella-120b,60.69
+wolfram/miquliz-120b-v2.0,54.57
+migtissera/Tess-72B-v1.5b,59.57
+vilm/Quyen-Pro-v0.1,47.3
+google/gemma-7b-it,24.85
+google/gemma-2b-it,24.16
+abacusai/Smaug-72B-v0.1,60.22
+yam-peleg/Experiment26-7B,38.93
+cognitivecomputations/Samantha-120b,48.58
+sophosympatheia/Midnight-Miqu-70B-v1.0,40.74
+claude-3-sonnet-20240229,61.01
+Infinimol/miiqu-f16,63.28
+mlabonne/Beyonder-4x7B-v3,39.03`;
+
+function setupDarkModeToggle() {
+	var toggle = document.getElementById('darkModeToggle');
+	var label = document.getElementById('toggleLabel');
+	
+	// Check if a preference is saved in localStorage and apply it
+	const savedMode = localStorage.getItem('darkModeEnabled');
+	if (savedMode !== null) {
+		const isDarkMode = savedMode === 'true';
+		document.body.classList.toggle('dark-mode', isDarkMode);
+		toggle.checked = isDarkMode;
+		label.textContent = isDarkMode ? 'Dark' : 'Light';
+	}
+
+	toggle.addEventListener('change', function() {
+		document.body.classList.toggle('dark-mode', this.checked);		 
+		label.textContent = this.checked ? 'Dark' : 'Light';
+		localStorage.setItem('darkModeEnabled', this.checked); // Save the current preference
+	});
+}
+
+function applySystemTheme() {
+	const toggle = document.getElementById('darkModeToggle');
+	const label = document.getElementById('toggleLabel');
+
+	// Apply system theme only if no saved preference
+	if (localStorage.getItem('darkModeEnabled') === null) {
+		const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		document.body.classList.toggle('dark-mode', prefersDarkMode);
+		toggle.checked = prefersDarkMode;
+		label.textContent = prefersDarkMode ? 'Dark' : 'Light';
+	} else {
+		// If there is a saved preference, ensure it's applied correctly
+		const isDarkMode = localStorage.getItem('darkModeEnabled') === 'true';
+		document.body.classList.toggle('dark-mode', isDarkMode);
+		toggle.checked = isDarkMode;
+		label.textContent = isDarkMode ? 'Dark' : 'Light';
+	}
+}
+
+
+function displayEncodedEmail() {
+	var encodedUser = '&#99;&#111;&#110;&#116;&#97;&#99;&#116;';
+	var encodedDomain = '&#101;&#113;&#98;&#101;&#110;&#99;&#104;&#46;&#99;&#111;&#109;';
+	var emailElement = document.getElementById('email');
+	emailElement.innerHTML = decodeHtmlEntities(encodedUser + '&#64;' + encodedDomain);
+
+	var emailAddress = emailElement.innerText;
+	emailElement.innerHTML = `<a href="mailto:${emailAddress}">Contact</a>`;
+}
+
+function decodeHtmlEntities(encodedString) {
+	var textArea = document.createElement('textarea');
+	textArea.innerHTML = encodedString;
+	return textArea.value;
+}
+
+
+$.fn.dataTable.ext.type.order['scores-pre'] = function (data) {
+	// Handle missing or null values;
+	if (!data || data === '-') {
+		 return -1; // Always sort these values last
+	}
+
+	return parseFloat(data) || 0;
+};
+
+$.fn.dataTable.ext.type.order['params-pre'] = function (data) {
+	if (!data || data === '-') {
+		return 9999; // Sort missing or null values last
+  }
+  if (data.includes('x')) {
+		const parts = data.split('x').map(Number);
+		return parts.reduce((acc, val) => acc * val, 1); // Multiply if in 'x' format
+  }
+  return parseFloat(data) || 0; // Default to float conversion
+};
+
+
+
+
+function loadLeaderboardData() {
+	const eqbenchRows = leaderboardDataEqbench.split('\n').slice(1); // Skip header for EQ-Bench data
+	const magiRows = leaderboardDataMagi.split('\n').slice(1).map(row => {
+		 const [model, score] = row.split(',');
+		 return { model, score: parseFloat(score) };
+	});
+
+	// Calculate max scores for each series
+	const maxScoreEQBench = Math.max(...eqbenchRows.map(row => parseFloat(row.split(',')[1])));
+	const maxScoreMagi = Math.max(...magiRows.map(row => row.score));
+
+	let html = eqbenchRows.map(eqbenchRow => {
+		const [modelName, score, parameters] = eqbenchRow.split(',');
+		const cleanModelName = modelName.replace(/^\*/, ''); // Remove leading asterisk
+		const isNewModel = modelName.startsWith('*'); // Check if the model is new
+		const magiEntry = magiRows.find(magiRow => magiRow.model === cleanModelName);
+
+		const magiScore = magiEntry ? magiEntry.score : 0; // Use 0 if MAGI score is missing
+		const scoreNum = parseFloat(score);
+		const combined = magiScore ? ((scoreNum + magiScore) / 2).toFixed(2) : 0;
+		
+		// Calculate score percentages based on their respective max scores
+		let scorePercentageEQ = (scoreNum / maxScoreEQBench) * 100;
+		let scorePercentageMagi = magiEntry ? (magiScore / maxScoreMagi) * 100 : 0;
+
+		let maxScoreCombined = Math.max(...eqbenchRows.map(row => {
+			let score = parseFloat(row.split(',')[1]);
+			let magiScore = magiRows.find(magiRow => magiRow.model === row.split(',')[0])?.score || 0;
+			return magiScore ? ((score + magiScore) / 2) : 0;
+		}));
+		let scorePercentageCombined = ((parseFloat(combined) / maxScoreCombined) * 100) || 0;
+
+		// Extract model name without creator
+		let displayModelName = cleanModelName.split('/').pop();
+		let modelNameDisplay = cleanModelName.includes('/') 
+							? `<a href="https://huggingface.co/${cleanModelName}" target="_blank">${displayModelName}</a>` 
+							: displayModelName;
+		if (isNewModel) {
+			modelNameDisplay = '🆕' + modelNameDisplay
+		}
+		
+		let scoreBarEQ = `
+		<div class="score-bar-container">
+			<div class="score-bar" style="width: ${scorePercentageEQ}%"></div>
+			<span class="score-text">${score}</span>
+		</div>
+		`;		 
+
+		let scoreBarMagi = magiEntry ? `<div class="score-bar-container">
+				<div class="score-bar" style="width: ${scorePercentageMagi}%"></div>		
+				<span class="score-text">${magiScore}</span>		
+		</div>
+		` : `<span class="score-text"></span>`;
+
+		let scoreBarCombined = combined ? `<div class="score-bar-container">
+		<div class="score-bar" style="width: ${scorePercentageCombined}%"></div>		
+		<span class="score-text">${combined}</span>
+		</div>
+		` : `<span class="score-text"></span>`;
+
+		return `<tr class="${''}">
+			<td>${modelNameDisplay}</td>
+			<td>${parameters}</td>
+			<td data-order="${score}">${scoreBarEQ}</td>
+			<td data-order="${magiScore}">${scoreBarMagi}</td>
+			<td data-order="${combined}">${scoreBarCombined}</td>
+			</tr>`;
+	}).join('');
+	
+	document.getElementById('leaderboardBody').innerHTML = html;
+	initializeDataTable();
+}
+
+
+function initializeDataTable() {
+	let table = $('#leaderboard').DataTable({
+		 "order": [[4, "desc"]], // Default sorting
+		 "pageLength": 100,
+		 "lengthMenu": [50, 100, 200, 1000],
+		 "language": {
+			  "lengthMenu": "Show _MENU_"
+		 },
+		 "columnDefs": [
+				{ "targets": [2, 3, 4], "orderSequence": ["desc", "asc"] }, // For score columns: sort desc first
+				{
+					"targets": [1], // Adjust this index based on your table's structure
+            	"type": "params" // Use the custom sorting type defined above
+				},
+				{
+					"targets": [3,4], // Index of the MAGI & Combined columns
+					"type": "scores"
+				},
+		],
+		 "dom": "<'d-flex flex-column flex-md-row justify-content-between'<'dataTables_length'l><'dataTables_filter'f>>" +
+				  "<'row'<'col-12'tr>>" +
+				  "<'row'<'col-md-5'i><'col-md-7'p>>",
+		"drawCallback": function(settings) {
+					// Hide all score bars initially
+					$('.score-bar').hide();
+			  
+					let api = this.api();
+					let sortedColumnIndex = api.order()[0][0];
+					const SCORE_COLUMNS = [2, 3, 4]; // Indices for the score columns (EQ-Bench, MAGI, and Avg)
+					const MODEL_PARAMS_COLUMNS = [0, 1]; // Indices for the Model and Params columns
+			  
+					// Check if the sorted column is a score column
+					if (SCORE_COLUMNS.includes(sortedColumnIndex)) {
+						 // Show score bar for the sorted score column only
+						 api.cells(null, sortedColumnIndex).nodes().to$().find('.score-bar').show();
+			  
+						 // Adjust the width of the sorted score column
+						 $('th').css('width', ''); // Reset widths for all headers
+						 $(api.column(sortedColumnIndex).header()).css('width', '33%');
+			  
+						 // Update the last sorted score column
+						 lastSortedScoreColumn = sortedColumnIndex;
+					} else if (MODEL_PARAMS_COLUMNS.includes(sortedColumnIndex)) {
+						 // If sorted by Model or Params, do not reset the score bars and column width
+						 if (lastSortedScoreColumn !== null) {
+							  // Reapply the width adjustment to the last sorted score column
+							  $('th').css('width', '');
+							  $(api.column(lastSortedScoreColumn).header()).css('width', '33%');
+			  
+							  // Make the score bar of the last sorted score column visible
+							  api.cells(null, lastSortedScoreColumn).nodes().to$().find('.score-bar').show();
+						 }
+					}
+			  }
+    });
+}
+
+
+
+let lastSortedScoreColumn = null;
+
+function adjustScoreBarsAndColumnWidth(table, sortedColumnIndex) {
+    const SCORE_COLUMNS = [2, 3, 4];
+    const MODEL_PARAMS_COLUMNS = [0, 1];
+
+    // Reset width adjustments for all columns
+    $('th').css('width', '');
+
+    // Determine action based on the sorted column
+    if (SCORE_COLUMNS.includes(sortedColumnIndex)) {
+        // Sorting by a score column
+        // Show the score bar for the sorted score column
+        $('.score-bar').css('display', 'none');
+        table.columns(sortedColumnIndex).nodes().flatten().to$().find('.score-bar').css('display', 'block');
+        
+        $(table.column(sortedColumnIndex).header()).css('width', '33%');
+
+        lastSortedScoreColumn = sortedColumnIndex;
+    } else if (MODEL_PARAMS_COLUMNS.includes(sortedColumnIndex)) {
+        // Sorting by Model or Params column
+        // Do not adjust width, but maintain the state of score bars
+        if (lastSortedScoreColumn !== null) {
+            $('.score-bar').css('display', 'none');
+            table.columns(lastSortedScoreColumn).nodes().flatten().to$().find('.score-bar').css('display', 'block');
+        }
+    }
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	// Always execute
+	displayEncodedEmail();
+	//setupDarkModeToggle();
+	//applySystemTheme();
+
+	// Conditional execution based on the presence of elements
+	if (document.getElementById('leaderboard')) {
+		 loadLeaderboardData(); // Only load leaderboard data if the leaderboard element exists
+	}
+
+	
+
+	// This checks if the system theme preference should be applied, which is common functionality
+	applySystemTheme();
+
+	setupDarkModeToggle();
+
+	// Handle expandable citations in the about page
+	const expandoBtn = document.getElementById('expando-btn');
+	if (expandoBtn) {
+		 const expandoContent = document.querySelector('.expando-content');
+		 expandoContent.style.display = 'none';
+		 expandoBtn.textContent = 'Click to show citations';
+
+		 expandoBtn.addEventListener('click', function() {
+			  if (expandoContent.style.display === 'none' || expandoContent.style.display === '') {
+					expandoContent.style.display = 'block';
+					expandoBtn.textContent = 'Click to hide citations';
+					expandoContent.scrollIntoView({ behavior: 'smooth', block: 'start' });
+			  } else {
+					expandoContent.style.display = 'none';
+					expandoBtn.textContent = 'Click to show citations';
+			  }
+		 });
+	}
+});
+
+$(document).ready(function() {
+	$('#darkModeToggle').change(function() {
+		if ($(this).is(':checked')) {
+			$('body').addClass('dark-mode').removeClass('light-mode');
+			$('#toggleLabel').text('Dark');
+		} else {
+			$('body').addClass('light-mode').removeClass('dark-mode');
+			$('#toggleLabel').text('Light');
+		}
+	});
+});
